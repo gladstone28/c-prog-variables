a051079f43eb746906cbabb9481d173878b62559
gladstone28 <gladstonerose@hotmail.com>
2022-03-16 08:44:28 -0500

    added file named anotherexample.c

---
+++ anotherexample.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+  
+  int variableInt;
+  char variableChar;
+  double variableDouble;
+
+  // Feel free to play around with what you have learned about variables here. You can also create new ones if you like, but right now only the ones created for you are set to be displayed.
+variableInt = 20;
+variableChar = 'X';
+variableDouble = 10.67;
+
+
+
+  // Output logic
+  printf("display an int here: %d\n", variableInt);
+  printf("display a char here: %c\n", variableChar);
+  printf("display a double with 2 decimal places here: %.2f\n", variableDouble);
+
+}
